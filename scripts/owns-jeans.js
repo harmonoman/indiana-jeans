118fd502205a0ba097fe125e5aa7fef6fbd5deae
Mark Bond <markbond615@gmail.com>
2025-03-18 11:16:37 -0500

    Added transient state tracker

--- scripts/owns-jeans.js
+++ scripts/owns-jeans.js
@@ -8,9 +8,26 @@
            radio buttons for "yes and "no"
 */
 
+import { setOwnsBlueJeans } from "./transient-state.js";
+
 export const OwnJeansChoices = () => {
+
+    document.addEventListener("change", handleOwnershipChange);
+
     let html = "<h2>Do you own a pair of blue jeans?</h2>"
     html += "<input type='radio' name='ownsJeans' value='true' /> Yes";
     html += "<input type='radio' name='ownsJeans' value='false' /> No";
     return html;
+}
+
+/*
+    Since the value of the radio buttons is the string of 'true' and 
+    the string of 'false', you must convert the string to an actual
+    boolean with JSON.parse() as seen below
+*/
+const handleOwnershipChange = (changeEvent) => {
+    if (changeEvent.target.name === "ownsJeans") {
+        const convertedToBoolean = JSON.parse(changeEvent.target.value);
+        setOwnsBlueJeans(convertedToBoolean);
+    }
 }
