54d6e9c2405b0b14eca4d1ddfa7b33491a4e6db6
Mark Bond <markbond615@gmail.com>
2025-03-30 12:51:54 -0500

    Added ability to display submissions

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,6 @@
 import { OwnJeansChoices } from "./owns-jeans.js"
 import { saveSubmission } from "./saveSubmission.js";
+import { SubmissionList } from "./submissions.js";
 import { LocationTypeChoices } from "./urban-dweller.js"
 
 const container = document.querySelector("#container")
@@ -8,12 +9,18 @@ const render = async () => {
     const jeanOwnershipHTML = OwnJeansChoices();
     const locationsHTML = await LocationTypeChoices();
     const buttonHTML = saveSubmission();
+    const submissionListHTML = await SubmissionList();
 
-    container.innerHTML = `   
-        ${jeanOwnershipHTML}
-        ${locationsHTML}
-        ${buttonHTML}
-    `
+    container.innerHTML = ` 
+        <div class="questions-container">
+            ${jeanOwnershipHTML}
+            ${locationsHTML}
+            ${buttonHTML}
+        </div>
+        <div class="submissions-container">
+            ${submissionListHTML}
+        </div>
+    `;
 }
 
 render();
