6e29450adcaca4d54be689924aeb4d7f70f771c6
Mark Bond <markbond615@gmail.com>
2025-04-07 14:43:03 -0500

    Converted transientState from a JSON object into a Map

--- scripts/transient-state.js
+++ scripts/transient-state.js
@@ -1,31 +1,30 @@
 // Set up the transient state data structure and provide initial values
-const transientState = {
-    ownsBlueJeans: false,
-    socioLocationId: 0
-}
-
+const transientState = new Map([
+    ["ownsBlueJeans", false],
+    ["socioLocationId", 0]
+]);
 
 // Functions to modify each property of transient state
 export const setOwnsBlueJeans = (chosenOwnership) => {
-    transientState.ownsBlueJeans = chosenOwnership;
+    transientState.set("ownsBlueJeans", chosenOwnership);
 }
-
 
 // Function to convert transient state to permanent state
 export const setSocioLocationId = (chosenLocation) => {
-    transientState.socioLocationId = chosenLocation;
+    transientState.set("socioLocationId", chosenLocation);
 }
 
+// const resetTransientState = () => {
 const resetTransientState = () => {
-    transientState.ownsBlueJeans = false,
-    transientState.socioLocationId = 0
+    transientState.set("ownsBlueJeans", false)
+    transientState.set("socioLocationId", 0)
 }
 
 export const saveSurveySubmission = async () => {
     const postOptions = {
         method: "POST",
         headers: { "Content-Type": "application/json" },
-        body: JSON.stringify(transientState)
+        body: JSON.stringify(Object.fromEntries(transientState))
     }
 
     const response = await fetch("http://localhost:8088/submissions", postOptions);
